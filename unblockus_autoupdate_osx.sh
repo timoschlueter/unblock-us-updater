a5e893748aac2bff53098e09daf67b75fa460b35
Edward Coleridge Smith <edcoleridgesmith@hotmail.com>
2015-03-03 21:52:29 +0000

    Create unblockus_autoupdate_osx.sh

---
+++ unblockus_autoupdate_osx.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#
+# Unblock-Us Update-Script
+#
+# This script automatically sends your current IP adress to the Unblock-Us api.
+# It can be used to update your IP adress via cron.
+#
+# Author:       Timo Schlueter
+# Mail:         me@timo.in
+# Web:          www.timo.in
+# Twitter:      twitter.com/tmuuh
+#
+# Version:      0.3
+# Date:         03-03-2015
+#
+# Notes:        I am not affiliated with Unblock-Us
+#
+
+# Variables (user specific)
+userlogin="email@example.com"
+userpassword="password"
+
+# Environment
+apiurl="https://api.unblock-us.com/login?$userlogin:$userpassword"
+
+# Check if username and password are set.
+if [ -z $userlogin ]
+  then
+  echo "No username set."
+  exit 1
+elif [ -z $userpassword ]
+  then
+  echo "No password set."
+  exit 1
+else
+  # Call the api
+  response=$(curl --silent $apiurl)
+
+  # Check response from api
+  if [ "$response" = "active" ]; then
+    echo "IP address is active. You are good to go!"
+    exit 0
+  elif [ "$response" = "bad_password" ]; then
+    echo "Wrong username or password."
+    exit 1
+  elif [ "$response" = "not_found" ]; then
+    echo "Username not found."
+    exit 1
+  else
+    echo "Unknown error. Check api url or documentantion."
+    exit 1
+  fi
+fi
